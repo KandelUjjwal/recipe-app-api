c22218c723f9f77046e67c926f7cf843bdb396a7
Ujjwal Kandel <ujjwal.kandel@maitriservices.com>
2025-04-17 04:19:13 +0000

    feat: added test files to check if tests run correctly

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Adds x and y and returns result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtracts x from y and returns result."""
+    return y - x
